e48ae7eb3ff05aeb5f47016fe8e44c6341e7d005
Atta ✨ <hi@attacomsian.com>
2022-10-01 11:39:51 +0500

    Fix file size typo

--- nodejs/express-multer/index.js
+++ nodejs/express-multer/index.js
@@ -15,7 +15,7 @@ const upload = multer({
     dest: `${FILE_PATH}/`,
     limits: {
         files: 5, // allow up to 5 files per request,
-        fieldSize: 2 * 1024 * 1024 // 2 MB (max file size)
+        fileSize: 2 * 1024 * 1024 // 2 MB (max file size)
     },
     fileFilter: (req, file, cb) => {
         // allow images only
